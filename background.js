a8257789f078dc4c80a704af975ddb2b5d511fa0
dylanlott <lott.dylan@gmail.com>
2015-06-09 13:30:35 -0600

    updates to background.js

---
+++ background.js
@@ -0,0 +1,18 @@
+var q = require('q'); 
+
+//Create new GET
+var xhr = new XMLHttpRequest();
+
+//GET request for Bookmarks
+var bookmarkTreeNodes = chrome.bookmarks.getTree(
+  function(bookmarkTreeNodes) {
+     console.log(bookmarkTreeNodes);
+  });
+}
+
+//send Bookmarks data to server 
+$.ajax({
+  type: 'POST', 
+  url: '//localhost:8080/api/bookmarks'
+  data: [bookmarkTreeNodes]
+}); 
